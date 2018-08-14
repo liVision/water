5e48eeceddc2145b377c888f94f79f153c909445
李文举 <>
2018-08-14 22:05:46 +0800

    ..

--- js/index.js
+++ js/index.js
@@ -8,29 +8,50 @@ $(document).ready(function() {
   $(".p-raise").on('click', function() {
     window.location.href = "./login.html";
   });
-  $(".title").on('click', function() {
-    $('.overlay').css({"opacity":"1","display":"flex"});
-    $('.dialog').fadeIn(500);
+  $(".title-right").on('click', function() {
+    $('.baozhang').css({"opacity":"1","display":"flex"});
+    $('.baozhang-dialog').fadeIn(500);
   });
   $(".close-btn").on('click', function() {
-    $('.overlay').css({"opacity":"0","display":"none"});
-    $('.dialog').fadeOut(500);
+    $('.baozhang').css({"opacity":"0","display":"none"});
+    $('.baozhang-dialog').fadeOut(500);
   });
   $(".check").on('click', function() {
-    $('.overlay').css({"opacity":"0","display":"none"});
-    $('.dialog').fadeOut(500);
+    $('.baozhang').css({"opacity":"0","display":"none"});
+    $('.baozhang-dialog').fadeOut(500);
   });
-  $(".div-spread").on('click', function() {
+  $(".story-spread").on('click', function() {
     if ($(".text-content").hasClass('short')) {
-      $(".text-content").removeClass('short')
+      $(".text-content").removeClass('short');
+      $(".story-spread span").text("点击收起");
     } else {
       $(".text-content").addClass('short')
+      $(".story-spread span").text("展开全文");
     }
   });
+  $(".jinzhan-spread").on('click', function() {
+    if ($(".jinzhan").hasClass("none")) {
+      $(".jinzhan").removeClass("none");
+      $(".jinzhan").css("display", "flex")
+      $(".jinzhan-spread").text("收起")
+    } else {
+      $(".jinzhan").addClass("none");
+      $(".jinzhan").css("display", "none")
+      $(".jinzhan-spread").text("展开")
+    }
+  })
   $(".p-title-report").on('click', function() {
     window.location.href = "./zhiyi.html";
   });
   $(".report-right").on('click', function() {
     window.location.href = "./zhiyi.html";
+  });
+  $(".div-huzhu-explain").on('click', function() {
+    $('.explain-text').css({"opacity":"1","display":"flex"});
+    $('.explain-dialog').fadeIn(500);
+  });
+  $(".dialog a").on('click', function() {
+    $('.explain-text').css({"opacity":"0","display":"none"});
+    $('.explain-dialog').fadeOut(500);
   });
 });
